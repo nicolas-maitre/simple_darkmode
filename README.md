4c6cea5b50d5621948990e9271a18e2c2a364c42
Nicolas Maitre <nicolas.johan.maitre@gmail.com>
2020-04-03 11:05:54 +0200

    improved README + format code + new consts

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # simple_darkmode
-easy
+A darkmode, easy to implement
+
+## Dependancies
+You will need at least the following methods from my (and included) `utils.js` library:
+- Cookies.get
+- Cookies.set
+- async_requestAnimationFrame
+- async_setTimeout
+- HTMLCollection.prototype.forEach
+
+## How to use
+
+**darkmode switch**  
+You will need a targetable **checkbox** in your website that will be used as a darkmode switch.  
+You can configure its selector in the `SWITCH_SELECTOR` constant.  
+The project comes with an already designed checkbox.
+
+**basic use**  
+Most of the design change should be done using css selectors.  
+To do that, the `lightMode` and `darkMode` classes will be toggled on the document body.  
+To allow animations, the `switchingMode` class will be added during mode change.  
+Look at the provided webpage for examples.  
+You should use css variables to simplify mode change.  
+
+**switching attributes**  
+You will need to add `dark_{attribute name}` or `light_{attribute name}`  
+Make sure that the attribute you are using is present in the `SWITCHABLE_ATTRIBUTES` constant. (if not, add it)   
+The darkmode will replace the specified attributre with your value, depending on the display mode.
+
+**switching classes**  
+If you don't want the whole `class` attribute to be overwritten everytime but only want to toggle a class, you can add the `dark_toggle_class` or `light_toggle_class` attribute to your elements.  
+It will only remove/add the specified class.
+
+## Adding custom actions
+You can add extra code to further customise the experience in:
+- `animateModeChange()`: to customize the animation
+- `displayMode()`: to customize how elements switched from mode to mode / add special cases
+
+## Switching mode with javascript
+The `Darkmode` object contains the `setMode` method than you can call with the mode (light/dark) as parameter to switch mode.
--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -1,43 +1,49 @@
 "use strict"
-/*  Original creator: Nicolas Maitre
+/*  
+*   Repo: https://github.com/nicolas-maitre/simple_darkmode.git
+*   Original creator: Nicolas Maitre
 *   Date: 08.01.2020
 *   Requires: utils.js */
-var DarkMode = new(function(){
+var DarkMode = new (function () {
     var _this = this;
-    
-    var LIGHT_MODE_CLASS = "lightMode";
-    var DARK_MODE_CLASS = "darkMode";
-    var SWITCH_ANIMATION_CLASS = "switchingMode";
-    var OPPOSITE_MODE = {dark: "light", light: "dark"};
+
+    const LIGHT_MODE_CLASS = "lightMode";
+    const DARK_MODE_CLASS = "darkMode";
+    const SWITCH_ANIMATION_CLASS = "switchingMode";
+    const TOGGLE_CLASS_SUFFIX = "_toggle_class";
+    const OPPOSITE_MODE = { dark: "light", light: "dark" };
+    const SWITCH_SELECTOR = ".darkModeSelector > input";
+    const COOKIE_NAME = "lightMode";
+    const SWITCHABLE_ATTRIBUTES = ["src", "class", "style"];
+    const ANIMATION_LENGTH = 500; //should also be changed in css
 
     var darkModeSelector = false;
     var isSwitchingMode = false;
 
     document.addEventListener("DOMContentLoaded", boot);
-    function boot(){
+    function boot() {
         //get input
-        darkModeSelector = document.querySelector(".darkModeSelector > input");
-
+        darkModeSelector = document.querySelector(SWITCH_SELECTOR);
+        
         //load from cookie
-        var mode = Cookies.get()["lightMode"];
-        if(mode){
+        var mode = Cookies.get()[COOKIE_NAME];
+        if (mode) {
             _this.setMode(mode, false);
         }
-
         //add input event
-        darkModeSelector.addEventListener("change", function(evt){
-            var mode = darkModeSelector.checked?"dark":"light";
+        darkModeSelector.addEventListener("change", evt => {
+            var mode = darkModeSelector.checked ? "dark" : "light";
             _this.setMode(mode);
         });
     }
 
     //set actions
-    this.setMode = function(mode, animate=true){
-        darkModeSelector.checked = (mode=="dark");
-        
-        Cookies.set("lightMode", mode);
-        
-        if(animate){
+    this.setMode = function (mode, animate = true) {
+        darkModeSelector.checked = (mode == "dark");
+
+        Cookies.set(COOKIE_NAME, mode);
+
+        if (animate) {
             animateModeChange(mode);
         } else {
             displayMode(mode);
@@ -45,7 +51,7 @@ var DarkMode = new(function(){
     }
 
     //animates change
-    async function animateModeChange(mode){
+    async function animateModeChange(mode) {
         //animation class
         await async_requestAnimationFrame();
         isSwitchingMode = true;
@@ -55,67 +61,70 @@ var DarkMode = new(function(){
         displayMode(mode);
         isSwitchingMode = false;
         //remove animation
-        await async_setTimeout(500);
+        await async_setTimeout(ANIMATION_LENGTH);
         await async_requestAnimationFrame();
-        if(!isSwitchingMode){
+        if (!isSwitchingMode) {
             document.body.classList.remove(SWITCH_ANIMATION_CLASS);
         }
     }
 
-    function displayMode(mode){
+    function displayMode(mode) {
         //body
-        switch(mode){
-            case "dark": 
-                document.body.classList.add(DARK_MODE_CLASS); 
-                document.body.classList.remove(LIGHT_MODE_CLASS); 
+        switch (mode) {
+            case "dark":
+                document.body.classList.add(DARK_MODE_CLASS);
+                document.body.classList.remove(LIGHT_MODE_CLASS);
                 break;
-            case "light": 
+            case "light":
                 document.body.classList.remove(DARK_MODE_CLASS);
                 document.body.classList.add(LIGHT_MODE_CLASS);
                 break;
             case "toggle":
-            default: 
-                document.body.classList.toggle(DARK_MODE_CLASS); 
-                document.body.classList.toggle(LIGHT_MODE_CLASS); 
-            break;
+            default:
+                document.body.classList.toggle(DARK_MODE_CLASS);
+                document.body.classList.toggle(LIGHT_MODE_CLASS);
+                break;
         }
         //alt attr
-        swapModeAttributes("src", mode);
-        swapModeAttributes("class", mode);
-        swapModeAttributes("style", mode);
+        SWITCHABLE_ATTRIBUTES.forEach(attr => {
+            swapModeAttributes(attr, mode);
+        });
         //alt class
         modeClassToggler(mode);
-        //other
+
+        //other actions:
 
     }
 
-    function swapModeAttributes(attribute, mode){
+    function swapModeAttributes(attribute, mode) {
         var fromAttr = `${OPPOSITE_MODE[mode]}_${attribute}`; //f.e. dark_src
         var toAttr = `${mode}_${attribute}`; //f.e light_src
         var elems = document.querySelectorAll(`*[${fromAttr}], *[${toAttr}]`);
         elems.forEach((elem) => {
-            if(!elem.hasAttribute(toAttr)){
+            if (!elem.hasAttribute(toAttr)) {
                 return; //no special attr
             }
-            if(!elem.hasAttribute(fromAttr) && elem.hasAttribute(attribute)){
+            if (!elem.hasAttribute(fromAttr) && elem.hasAttribute(attribute)) {
                 elem.setAttribute(fromAttr, elem.getAttribute(attribute)); //set opposite mode attribute if empty
             }
             elem.setAttribute(attribute, elem.getAttribute(toAttr));
         });
     }
 
-    function modeClassToggler(mode){
-        var toggleAttrSuffix = "_toggle_class";
+    function modeClassToggler(mode) {
+        var toggleAttrSuffix = TOGGLE_CLASS_SUFFIX;
         var removeToggleAttr = OPPOSITE_MODE[mode] + toggleAttrSuffix;
         var addToggleAttr = mode + toggleAttrSuffix;
         var elems = document.querySelectorAll(`*[${removeToggleAttr}], *[${addToggleAttr}]`);
         elems.forEach((elem) => {
-            if(elem.hasAttribute(removeToggleAttr)){
+            if (elem.hasAttribute(removeToggleAttr)) {
                 elem.classList.remove(elem.getAttribute(removeToggleAttr));
             }
-            if(elem.hasAttribute(addToggleAttr)){
+            if (elem.hasAttribute(addToggleAttr)) {
                 elem.classList.add(elem.getAttribute(addToggleAttr));
             }
         });
     }
 });
+
+// export default DarkMode;
